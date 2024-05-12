e6f77c71a3da08879942b98c29ba354524a5d4e4
Izzet Can Sen <56109327+IzzetCanSen@users.noreply.github.com>
2024-05-12 19:54:33 +0200

    Updating UI & created SuccessPopup component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,57 @@
-import { auth } from "../../firebaseConfig";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Image,
   StyleSheet,
   TextInput,
   TouchableOpacity,
   View,
+  Keyboard,
   Text,
+  Platform,
 } from "react-native";
-
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import SuccessPopup from "../../components/SuccessPopup"; // Adjust the path accordingly
 
 export default function HomeScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signinEmail, setSigninEmail] = useState("");
+  const [signinPassword, setSigninPassword] = useState("");
+  const [showSignInForm, setShowSignInForm] = useState(true);
+  const [successPopupVisible, setSuccessPopupVisible] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [keyboardIsShown, setKeyboardIsShown] = useState(false);
 
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      Platform.OS === "ios" ? "keyboardWillShow" : "keyboardDidShow",
+      () => {
+        setKeyboardIsShown(true);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      Platform.OS === "ios" ? "keyboardWillHide" : "keyboardDidHide",
+      () => {
+        setKeyboardIsShown(false);
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
 
   const handleSignUp = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed up
         const user = userCredential.user;
-        console.log(user);
+        setSuccessMessage("Signed up successfully!");
+        setSuccessPopupVisible(true);
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -41,11 +62,11 @@ export default function HomeScreen() {
   };
 
   const handleSignIn = () => {
-    signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    signInWithEmailAndPassword(auth, signinEmail, signinPassword)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log("Logged in");
-        console.log(user);
+        setSuccessMessage("Signed in successfully!");
+        setSuccessPopupVisible(true);
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -55,91 +76,161 @@ export default function HomeScreen() {
       });
   };
 
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <Text>Sign Up</Text>
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <TouchableOpacity style={styles.button} onPress={handleSignUp}>
-          <Text style={styles.buttonText}>Sign Up</Text>
-        </TouchableOpacity>
-      </View>
+  const closeSuccessPopup = () => {
+    setSuccessPopupVisible(false);
+  };
 
-      <Text>Sign In</Text>
-      <View style={styles.formContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={loginEmail}
-          onChangeText={setLoginEmail}
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={loginPassword}
-          onChangeText={setLoginPassword}
-          secureTextEntry
-        />
-        <TouchableOpacity style={styles.button} onPress={handleSignIn}>
-          <Text style={styles.buttonText}>Sign In</Text>
-        </TouchableOpacity>
-      </View>
-    </ParallaxScrollView>
+  return (
+    <View style={styles.authScreenContainer}>
+      {!keyboardIsShown && (
+        <Image source={require("@/assets/images/nourify-logo.png")} />
+      )}
+
+      {showSignInForm ? (
+        <View style={styles.formContainer}>
+          <Text style={styles.formTitle}>Sign In</Text>
+          <View style={styles.formInputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              placeholderTextColor="#fff"
+              value={signinEmail}
+              onChangeText={setSigninEmail}
+              keyboardType="email-address"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#fff"
+              value={signinPassword}
+              onChangeText={setSigninPassword}
+              secureTextEntry
+            />
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleSignIn}>
+            <Text style={styles.buttonText}>Sign In</Text>
+          </TouchableOpacity>
+          <View style={styles.formExtraInformation}>
+            <Text style={styles.formExtraInformationText}>
+              Don't have an account?
+            </Text>
+            <Text
+              style={styles.formExtraInformationButton}
+              onPress={() => setShowSignInForm(false)}
+            >
+              Sign Up
+            </Text>
+          </View>
+        </View>
+      ) : (
+        <View style={styles.formContainer}>
+          <Text style={styles.formTitle}>Sign Up</Text>
+          <View style={styles.formInputContainer}>
+            <TextInput
+              style={styles.input}
+              placeholder="Username"
+              placeholderTextColor="#fff"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              placeholderTextColor="#fff"
+              value={email}
+              onChangeText={setEmail}
+              keyboardType="email-address"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              placeholderTextColor="#fff"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+          </View>
+          <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+            <Text style={styles.buttonText}>Sign Up</Text>
+          </TouchableOpacity>
+          <View style={styles.formExtraInformation}>
+            <Text style={styles.formExtraInformationText}>
+              Already have an account?
+            </Text>
+            <Text
+              style={styles.formExtraInformationButton}
+              onPress={() => setShowSignInForm(true)}
+            >
+              Sign In
+            </Text>
+          </View>
+        </View>
+      )}
+
+      <SuccessPopup
+        visible={successPopupVisible}
+        onClose={closeSuccessPopup}
+        message={successMessage}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  authScreenContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#222E3A",
+    padding: 20,
+    paddingTop: 150,
+    gap: 50,
   },
   formContainer: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    gap: 50,
+    width: "100%",
+  },
+  formTitle: {
+    fontSize: 23,
+    fontWeight: "700",
+    color: "#fff",
+  },
+  formInputContainer: {
+    width: "100%",
   },
   input: {
     height: 40,
-    width: "80%",
-    borderColor: "gray",
-    borderWidth: 1,
+    width: "100%",
+    borderBottomColor: "#fff",
+    borderBottomWidth: 1,
     marginBottom: 20,
     paddingHorizontal: 10,
+    color: "#fff",
   },
   button: {
-    backgroundColor: "#007BFF",
+    backgroundColor: "#3fa1ca",
     paddingVertical: 10,
     paddingHorizontal: 20,
-    borderRadius: 5,
+    borderRadius: 50,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
     color: "white",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  formExtraInformation: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  formExtraInformationText: {
+    color: "#fff",
+    fontWeight: "300",
+  },
+  formExtraInformationButton: {
+    color: "#fff",
+    textDecorationLine: "underline",
   },
 });
